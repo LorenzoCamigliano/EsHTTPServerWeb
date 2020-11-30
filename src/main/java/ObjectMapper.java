677852ccb97f1e6e3257297276e638183b2f9499
Lorenzo Camigliano <lorenzo@gmail.com>
2020-11-30 21:43:08 +0100

    Esercizio HTTPServer

---
+++ src/main/java/ObjectMapper.java
@@ -0,0 +1,20 @@
+
+import java.io.File;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lorenzo
+ */
+class ObjectMapper {
+
+    PuntiVendita readValue(File file, Class<PuntiVendita> aClass) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ src/main/java/PuntiVendita.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class PuntiVendita {
+   
+    private int size;
+    private ArrayList <PuntiVendita> punti = new ArrayList <>();
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public ArrayList<PuntiVendita> getPunti() {
+        return punti;
+    }
+
+    public void setPunti(ArrayList<PuntiVendita> punti) {
+        this.punti = punti;
+    }
+    
+    
+    
+}
